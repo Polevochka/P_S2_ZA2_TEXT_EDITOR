20698bc53f1f5dec484755405a6bff326b0b7eea
ogurechik <37779327+ogurechik@users.noreply.github.com>
2019-03-31 19:00:05 +0300

    Add files via upload

---
+++ unit1.pas
@@ -0,0 +1,178 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls,
+  Menus;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    MainMenu1: TMainMenu;
+    Memo1: TMemo;
+    MenuItem1: TMenuItem;
+    MenuItem10: TMenuItem;
+    MenuItem2: TMenuItem;
+    MenuItem3: TMenuItem;
+    MenuItem4: TMenuItem;
+    MenuItem5: TMenuItem;
+    MenuItem6: TMenuItem;
+    MenuItem7: TMenuItem;
+    MenuItem8: TMenuItem;
+    MenuItem9: TMenuItem;
+    OpenDialog1: TOpenDialog;
+    SaveDialog1: TSaveDialog;
+    procedure FormCreate(Sender: TObject);
+    procedure MenuItem2Click(Sender: TObject);
+    procedure MenuItem3Click(Sender: TObject);
+    procedure MenuItem4Click(Sender: TObject);
+    procedure MenuItem5Click(Sender: TObject);
+    procedure MenuItem6Click(Sender: TObject);
+    procedure MenuItem7Click(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+// Наши собственные типы данных, используемые в приложении
+Type
+
+  // тип для отдельно взятого студента
+  Stud = Record
+    No       : integer;     // Номер (его ID)
+    Name     : string[12];  // Имя
+    Gr       : string[8];   // Группа
+    o1,o2,o3 : integer;     // Оценки
+  end;
+
+{Глобальные переменные}
+// Спецификация файла - его полное имя, которое ВИДНО ВО ВСЕХ процедурах
+var sf: string;
+
+{ TForm1 }
+
+{Особые действия при открытии}
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+  sf := '';   // Никакого файла мы ещё не открывали
+  // Каталоги для сохраненияи и открытия по умочанию (Папка проекта)
+  OpenDialog1.InitialDir:='';
+  SaveDialog1.InitialDir:='';
+end;
+
+{Создать}
+procedure TForm1.MenuItem2Click(Sender: TObject);
+begin
+  Memo1.Clear;  // Очищаем текстовый редактор, что равносильно изменению в Memo1
+  Memo1.Modified:= False;  // Но мы же ничего не поменяли, надо это указать
+  sf:='';                  // А у файла нет ещё имени
+  Form1.Caption:= 'Form1'; // Заголовок окна
+end;
+
+{Открыть}
+procedure TForm1.MenuItem3Click(Sender: TObject);
+begin
+  // Перед тем как открыть новый файл, нодо проверить вдруг есть несохранённые
+  // изменения в старом - УЖЕ открытом(новом пустом) файле
+  If Memo1.Modified then
+    case MessageDlg('Текст был изменён' + #13 + 'Сохранить его?',
+                    mtConfirmation,[mbYes, mbNo, mbCancel],0) of
+      mrYes   : MenuItem5Click(self); // Да -> сохраняем старый файл
+      mrNo    : ;
+      mrCancel: Exit;
+    end;
+
+  // Если диалог открытия файла завершился нормально,
+  // То есть его не закрыли и не нажали cancel
+  // То есть юзер выбрал нужный ему файл и нажал ОК
+  If openDialog1.Execute then
+    begin
+      sf:=OpenDialog1.FileName;     // Извлекаем имя файла из этого диалога
+      Memo1.Lines.LoadFromFile(sf); // Выводим его в Memo1
+      Memo1.Modified:=False;  // Что равносильно его изменению, но мы же не изменяли файл
+      Form1.Caption:='Form1 ' + sf; // В заголовок окна выводим имя файла
+    end;
+
+end;
+
+{Закрыть}
+procedure TForm1.MenuItem4Click(Sender: TObject);
+begin
+  // Стандартных диалог сохранения файла
+  // Если Memo1 было изменено
+  if Memo1.Modified then
+      // Стандартное окно Сообщения
+      case MessageDlg('Данные о студентах были изменены' + #13 + 'Сохранить их?',
+                      mtConfirmation,[mbYes, mbNo, mbCancel],0) of
+        mrYes: MenuItem5Click(self); // Сохраняем файл
+        mrNo:;                    // Ничего не делаем
+        mrCancel: Exit; // Выходим из окна сообщения, и возвращаемся к редактированию текста(действия ниже выполняться не будут)
+      end;
+
+  // Если мы не вишли через 'Cancel', то совершаем стандартные действия
+  Memo1.Clear;  // Очищаем окно редактора
+  Memo1.Modified:= False;
+  sf:='';
+  Form1.Caption:= 'Form1';
+
+end;
+
+{Сохранить как}
+procedure TForm1.MenuItem6Click(Sender: TObject);
+begin
+  // Если диалог сохранения прошёл хорошо
+  if SaveDialog1.Execute then
+    begin
+      sf:= SaveDialog1.FileName; // Извлекаем имя файла
+      Memo1.Lines.SaveToFile(sf); // Cохраняем содержимое редактора в файл с именем sf
+
+      Memo1.Modified := False; // Содержимое в редакторе соответсвует файлу на диске
+      Form1.Caption:= 'Form1 ' + sf; // Устанавливаем заголовок приложения с именем файла
+    end;
+
+end;
+
+{Сохранить}
+procedure TForm1.MenuItem5Click(Sender: TObject);
+begin
+  // Исли имя файла не задано, то вызываем Окно 'сохранить как'
+  if sf = '' then MenuItem6Click(self)
+  else  // Иначе, то есть имя файла уже установлено
+    begin
+      Memo1.Lines.SaveToFile(sf); // Сразу сохраняем его на диск
+      Memo1.Modified:= False;  // Содержание устанавливаем не изменённым, тк сохранили всё на диск
+    end;
+
+end;
+
+{Выход}
+procedure TForm1.MenuItem7Click(Sender: TObject);
+begin
+  // Сообщение: Сохранить ли изменённый файл
+  if Memo1.Modified then
+    case MessageDlg('Файл был изменён' + #13 + 'Сохранить его?',
+                    mtConfirmation,[mbYes, mbNo, mbCancel],0) of
+      mrYes   : MenuItem5Click(self); // Да -> сохраняем открытый файл
+      mrNo    : ;
+      mrCancel: Exit;
+    end;
+  // Закрываем приложение
+  close;
+end;
+
+
+end.
+
